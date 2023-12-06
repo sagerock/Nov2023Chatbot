914d8278949f75442e6033f55d67560985c0ed88
sagerock <sage@sagerock.com>
2023-12-06 14:58:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def send_message():
 
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-4",
+            model="gpt-3.5-turbo-1106",
             messages=[{"role": "user", "content": message}]
         )
         return jsonify(response), 200
